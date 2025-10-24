75a8b1188cedc9066283e8015a66259488d46a22
Areej <areejhamid8560@gmail.com>
2025-10-24 13:25:24 +0500

    Node.js calculator with Jest tests

---
+++ calculator.test.js
@@ -0,0 +1,22 @@
+const { add, subtract, multiply, divide } = require('./calculator');
+
+test('adds 2 + 3 = 5', () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test('subtracts 5 - 3 = 2', () => {
+  expect(subtract(5, 3)).toBe(2);
+});
+
+test('multiplies 4 * 2 = 8', () => {
+  expect(multiply(4, 2)).toBe(8);
+});
+
+test('divides 8 / 2 = 4', () => {
+  expect(divide(8, 2)).toBe(4);
+});
+
+test('throws error when dividing by zero', () => {
+  expect(() => divide(5, 0)).toThrow('Cannot divide by zero');
+});
+
